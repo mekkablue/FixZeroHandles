6aae2d6efcd986cfda5db0a4a05aeaf9ceae8af9
mekka blue <mekka@mekkablue.com>
2014-03-20 20:08:42 +0100

    Initial Release

---
+++ FixZeroHandles.glyphsFilter/Contents/Resources/FixZeroHandles.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import objc
+from Foundation import *
+from AppKit import *
+import sys, os, re
+
+GlyphsFilterProtocol = objc.protocolNamed( "GlyphsFilter" )
+
+class GlyphsFilterFixZeroHandles ( NSObject, GlyphsFilterProtocol ):
+	tunnifyLo = 0.43
+	tunnifyHi = 0.73
+	
+	def init( self ):
+		"""
+		Do all initializing here.
+		"""
+		return self
+	
+	def interfaceVersion( self ):
+		"""
+		Distinguishes the API version the plugin was built for. 
+		Return 1.
+		"""
+		return 1
+	
+	def setController_( self, Controller ):
+		"""
+		Do not touch this.
+		"""
+		try:
+			self._controller = Controller
+		except Exception as e:
+			self.logToConsole( "setController_: %s" % str(e) )
+	
+	def controller( self ):
+		"""
+		Do not touch this.
+		"""
+		try:
+			return self._controller
+		except Exception as e:
+			self.logToConsole( "controller: %s" % str(e) )
+		
+	def setup( self ):
+		"""
+		Do not touch this.
+		"""
+		try:
+			return None
+		except Exception as e:
+			self.logToConsole( "setup: %s" % str(e) )
+	
+	def title( self ):
+		"""
+		This is the human-readable name as it appears in the menu.
+		"""
+		return "Fix Zero Handles"
+	
+	def keyEquivalent( self ):
+		""" 
+		The key together with Cmd+Shift will be the shortcut for the filter.
+		Return None if you do not want to set a shortcut.
+		Users can set their own shortcuts in System Prefs.
+		"""
+		return None
+	
+	def processLayer( self, thisLayer, selectionCounts ):
+		"""
+		Each selected layer is processed here.
+		"""
+		try:
+			selection = thisLayer.selection()
+			if selection == (): #empty selection
+				selectionCounts = False
+			
+			for thisPath in thisLayer.paths:
+				numOfNodes = len( thisPath.nodes )
+				nodeIndexes = range( numOfNodes )
+		
+				for i in nodeIndexes:
+					thisNode = thisPath.nodes[i]
+			
+					if (thisNode in selection or not selectionCounts) and thisNode.type == 65:
+						if thisPath.nodes[i-1].type == 65:
+							segmentNodeIndexes = [ i-2, i-1, i, i+1 ]
+						else:
+							segmentNodeIndexes = [ i-1, i, i+1, i+2 ]
+				
+						for x in range(len(segmentNodeIndexes)):
+							segmentNodeIndexes[x] = segmentNodeIndexes[x] % numOfNodes
+				
+						thisSegment = [ [n.x, n.y] for n in [ thisPath.nodes[i] for i in segmentNodeIndexes ] ]
+						newHandles = self.tunnify( thisSegment )
+						if newHandles:
+							xHandle1, yHandle1, xHandle2, yHandle2 = newHandles
+							thisPath.nodes[ segmentNodeIndexes[1] ].x = xHandle1
+							thisPath.nodes[ segmentNodeIndexes[1] ].y = yHandle1
+							thisPath.nodes[ segmentNodeIndexes[2] ].x = xHandle2
+							thisPath.nodes[ segmentNodeIndexes[2] ].y = yHandle2
+			return True
+		except Exception as e:
+			self.logToConsole( "processLayer: %s" % str(e) )
+			return False
+	
+	def xyAtPercentageBetweenTwoPoints( self, firstPoint, secondPoint, percentage ):
+		"""
+		Returns the x, y for the point at percentage
+		(where 100 percent is represented as 1.0)
+		between NSPoints firstPoint and secondPoint.
+		"""
+		x = firstPoint.x + percentage * ( secondPoint.x - firstPoint.x )
+		y = firstPoint.y + percentage * ( secondPoint.y - firstPoint.y )
+		return x, y
+	
+	def tunnify( self, segment ):
+		"""
+		Calculates the average curvature for Bezier curve segment P1, P2, P3, P4,
+		and returns new values for P2, P3.
+		"""
+		try:
+			x1, y1 = segment[0]
+			x2, y2 = segment[1]
+			x3, y3 = segment[2]
+			x4, y4 = segment[3]
+			
+			if [x1, y1] == [x2, y2]:
+				xInt, yInt = x3, y3
+				firstHandlePercentage = self.tunnifyLo
+				secondHandlePercentage = self.tunnifyHi
+			elif [x3, y3] == [x4, y4]:
+				xInt, yInt = x2, y2
+				firstHandlePercentage = self.tunnifyHi
+				secondHandlePercentage = self.tunnifyLo
+			else:
+				# no zero handle
+				return False
+			
+			intersectionPoint = NSPoint( xInt, yInt )
+			segmentStartPoint = NSPoint( x1, y1 )
+			segmentFinalPoint = NSPoint( x4, y4 )
+			
+			firstHandleX, firstHandleY = self.xyAtPercentageBetweenTwoPoints( segmentStartPoint, intersectionPoint, firstHandlePercentage )
+			secondHandleX, secondHandleY = self.xyAtPercentageBetweenTwoPoints( segmentFinalPoint, intersectionPoint, secondHandlePercentage )
+			
+			return firstHandleX, firstHandleY, secondHandleX, secondHandleY
+		except Exception as e:
+			self.logToConsole( "tunnify: %s" % str(e) )
+	
+	def runFilterWithLayers_error_( self, Layers, Error ):
+		"""
+		Invoked when user triggers the filter through the Filter menu
+		and more than one layer is selected.
+		"""
+		try:
+			for k in range(len(Layers)):
+				Layer = Layers[k]
+				Layer.clearSelection()
+				self.processLayer( Layer, False )
+		except Exception as e:
+			self.logToConsole( "runFilterWithLayers_error_: %s" % str(e) )
+			
+	def runFilterWithLayer_error_( self, Layer, Error ):
+		"""
+		Invoked when user triggers the filter through the Filter menu
+		and only one layer is selected.
+		"""
+		try:
+			return self.processLayer( Layer, True )
+		except Exception as e:
+			self.logToConsole( "runFilterWithLayer_error_: %s" % str(e) )
+	
+	def processFont_withArguments_( self, Font, Arguments ):
+		"""
+		Invoked when called as Custom Parameter in an instance at export.
+		The Arguments come from the custom parameter in the instance settings. 
+		The first item in Arguments is the class-name. After that, it depends on the filter.
+		"""
+		try:
+			FontMasterId = Font.fontMasterAtIndex_(0).id
+			for thisGlyph in Font.glyphs:
+				Layer = thisGlyph.layerForKey_( FontMasterId )
+				self.processLayer( Layer, False )
+		except Exception as e:
+			self.logToConsole( "processFont_withArguments_: %s" % str(e) )
+	
+	def logToConsole( self, message ):
+		"""
+		The variable 'message' will be passed to Console.app.
+		Use self.logToConsole( "bla bla" ) for debugging.
+		"""
+		myLog = "Filter %s:\n%s" % ( self.title(), message )
+		NSLog( myLog )
---
+++ README.md
@@ -0,0 +1,40 @@
+# FixZeroHandles.glyphsFilter
+
+This is a plugin for the [Glyphs font editor](http://glyphsapp.com/) by Georg Seifert. It analyzes the path structure of selected layers and will rearrange path segments that contain completely retracted handles, a.k.a. ‘zero handles’. Zero handles typically appear in outlines imported from other vector apps, such as Adobe Illustrator. Zero handles are considered bad style, or even an error, and can cause a range of problems, especially when you are trying to convert your outlines to  TrueType curves.
+
+![Fixing Zero Handles: Before and After.](FixZeroHandles.png "Fix Zero Handles")
+
+Installation of this filter will add the menu item *Filter > Fix Zero Handles*. You can set a keyboard shortcut in System Preferences.
+
+### Requirements
+
+The filter needs Glyphs 1.4.3 or higher, running on OS X 10.7 or later. I tested it on OS X 10.9 only, and I assume it will not work in versions of Mac OS X older than 10.7.
+
+### Installation
+
+1. Download the complete ZIP file and unpack it, or clone the repository.
+2. Double click the .glyphsFilter file. Confirm the dialog that appears in Glyphs.
+3. Restart Glyphs
+
+### Usage Instructions
+
+1. Open a glyph in Edit View, or select any number of glyphs in Font or Edit View.
+2. Choose *Filter > Fix Zero Handles*.
+
+Alternatively, you can also use it as a custom parameter in an instance:
+
+	Property: Filter
+	Value: GlyphsFilterFixZeroHandles;
+
+### License
+
+Copyright 2014 Rainer Erich Scheichelbauer (@mekkablue).
+Based on sample code by Georg Seifert (@schriftgestalt).
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+See the License file included in this repository for further details.
